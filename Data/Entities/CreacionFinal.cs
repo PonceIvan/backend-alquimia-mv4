971eedbb8a91499fec1e48bf93c1844d0888f999
Axel <axeelleguero@gmail.com>
2025-05-03 16:40:05 -0300

    Deleted project for import error

--- Data/Entities/CreacionFinal.cs
+++
@@ -1,7 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace backendAlquimia.Data.Entities;
-public class CreacionFinal
-{
-    public Formula formula { get; set; }
-    public List<Producto> productos { get; set; }
-}
