5ae76b18f1e73d9d7ce872f588361607c5f9e807
Axel <axeelleguero@gmail.com>
2025-05-03 16:50:41 -0300

    Template de vuelta

---
+++ Data/Entities/TipoProducto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backendAlquimia.Data.Entities;
+
+public class TipoProducto
+{
+   
+    public int id { get; set; }
+    public string description { get; set; }
+}
