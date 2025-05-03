763398d35036e1b61c11b5ce6d1039599b598953
Axel <axeelleguero@gmail.com>
2025-05-03 19:09:58 -0300

    FEAT(entities) added Formula entity

---
+++ Data/Entities/Formula.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backendAlquimia.Data.Entities
+{
+    public class Formula
+    {
+        public int Id { get; set; }
+        [Required]
+        public Intensidad Intensidad { get; set; }
+        public Combinacion Combinacion { get; set; }
+
+        public Formula(Combinacion combinacion)
+        {
+            this.Combinacion = combinacion;
+        }
+    }
+}
