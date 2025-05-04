5524715f5b9d8582e822f489a4836ceb5624ca14
Axel <axeelleguero@gmail.com>
2025-05-04 15:03:53 -0300

    UPDATE(appsettings.json): added connection string

--- Data/Entities/Formula.cs
+++ Data/Entities/Formula.cs
@@ -5,8 +5,10 @@ namespace backendAlquimia.Data.Entities
     public class Formula
     {
         public int Id { get; set; }
+        public int IntensidadId { get; set; }
         [Required]
         public Intensidad Intensidad { get; set; }
+        public int CombinacionId { get; set; }
         public Combinacion Combinacion { get; set; }
 
         
