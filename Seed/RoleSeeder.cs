0e6c46ff51110d1cd2b5350ec0e897e7aaaeb129
Axel <axeelleguero@gmail.com>
2025-05-15 23:05:29 -0300

    FEAT(RoleSeeder) create class RoleSeeder to automatically create roles

---
+++ Seed/RoleSeeder.cs
@@ -0,0 +1,24 @@
+﻿using backendAlquimia.Data.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+
+namespace backendAlquimia.Seed
+{
+    public class RoleSeeder
+    {
+        private static readonly string[] Roles = { "Admin", "Creador", "Proveedor" };
+
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<Rol>>();
+
+            foreach (var role in Roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new Rol { Name = role });
+                }
+            }
+        }
+    }
+}
