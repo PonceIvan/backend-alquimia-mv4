a6643aa1a14a01fca80b87f61efe95522d7eeb11
Axel <axeelleguero@gmail.com>
2025-05-15 22:42:59 -0300

    UPDATE(Entities) deleted entities to implement roles

--- Data/Entities/Admin.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace backendAlquimia.Data.Entities;
-public class Admin : Usuario
-{
-    public int Id { get; set; }
-    public string Nombre { get; set; }
-}
