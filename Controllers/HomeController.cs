fd2d3fcff1c66d9df2f02e6053f1ef8fd0ff5b61
Axel <axeelleguero@gmail.com>
2025-05-17 21:07:57 -0300

    FEAT(HomeController): add endpoint to return user data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Security.Claims;
 using backendAlquimia.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,7 +7,7 @@ namespace backendAlquimia.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        /*private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,6 +28,31 @@ namespace backendAlquimia.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }*/
+        [HttpGet("/home/")]
+        public IActionResult ObtenerUsuarioInfo()
+        {
+            var identity = HttpContext.User.Identity;
+
+            if (identity != null && identity.IsAuthenticated)
+            {
+                var nombre = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+                var email = HttpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+                return Ok(new
+                {
+                    autenticado = true,
+                    nombre,
+                    email
+                });
+            }
+
+            return Ok(new
+            {
+                autenticado = false,
+                mensaje = "Usuario no autenticado"
+            });
         }
+
     }
 }
