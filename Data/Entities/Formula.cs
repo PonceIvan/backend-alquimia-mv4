ea1c959b8ebc1cf210240d73e17c8f88d9196ca1
Mica Zara <zara_micaela@outlook.com>
2025-05-18 00:15:19 -0300

    FEAT(entities: Formula y CreacionFinal): Unificar entidades Formula y CreacionFinal en Formula. Borrar CreacionFinal

--- Data/Entities/Formula.cs
+++ Data/Entities/Formula.cs
@@ -1,21 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace backendAlquimia.Data.Entities
+﻿namespace backendAlquimia.Data.Entities
 {
     public class Formula
     {
-
         public int Id { get; set; }
-
         public int CombinacionId { get; set; }
         public Combinacion Combinacion { get; set; }
-
         public int IntensidadId { get; set; }
         public Intensidad Intensidad { get; set; }
-
         public int CreadorId { get; set; }
         public Usuario Creador { get; set; }
-
+        public double ConcentracionAlcohol { get; set; }
+        public double ConcentracionAgua { get; set; }
+        public double ConcentracionEsencia { get; set; }
 
     }
 }
