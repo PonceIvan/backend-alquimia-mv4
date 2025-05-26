a4ce3ecef0a8e0cdc19c006f47bbeaad26149e0d
Mica Zara <zara_micaela@outlook.com>
2025-05-26 17:07:35 -0300

    FIX(api/creator): Get Base, Heart, Top Notes OK

---
+++ alquimia.Services/Services/Models/NoteDTO.cs
@@ -0,0 +1,13 @@
+﻿
+namespace backendAlquimia.Models
+{
+    public class NoteDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; internal set; }
+        public string Sector { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; internal set; }
+    }
+}
---
+++ alquimia.Services/Services/Models/NotesGroupedByFamilyDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace backendAlquimia.Models
+{
+    public class NotesGroupedByFamilyDTO
+    {
+        public string Family { get; set; }
+        public List<NoteDTO> Notes { get; set; }
+    }
+}
