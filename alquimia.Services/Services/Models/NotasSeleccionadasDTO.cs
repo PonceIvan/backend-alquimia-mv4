458f49cc56f677d92096b7ba63b3c553a70cbee2
Mica Zara <zara_micaela@outlook.com>
2025-05-28 00:53:29 -0300

    FIX(datatype): fix TimeOnly in OlfactoryPyramid and DTOs

--- alquimia.Services/Services/Models/NotasSeleccionadasDTO.cs
+++ alquimia.Services/Services/Models/NotasSeleccionadasDTO.cs
@@ -2,7 +2,6 @@
 {
     public class NotasSeleccionadasDTO
     {
-        // public int NuevaNotaId { get; set; }
         public List<int> ListaDeIdsSeleccionadas { get; set; }
         public string Sector { get; set; }
     }
