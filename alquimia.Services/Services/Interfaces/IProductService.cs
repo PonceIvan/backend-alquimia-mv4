2f0eb3dfc62b1e2a1e2d471c7e4f6634f9b12c0b
Mica Zara <zara_micaela@outlook.com>
2025-05-30 15:59:42 -0300

    FEAT(api): GET /product/price-range OK

--- alquimia.Services/Services/Interfaces/IProductService.cs
+++ alquimia.Services/Services/Interfaces/IProductService.cs
@@ -1,4 +1,6 @@
-﻿namespace backendAlquimia.alquimia.Services.Interfaces
+﻿using backendAlquimia.alquimia.Services.Models;
+
+namespace backendAlquimia.alquimia.Services.Interfaces
 {
     public interface IProductService
     {
@@ -7,7 +9,7 @@
         //Task<ProductoDTO> CrearProductoAsync(CreateProductoDTO dto, int idProveedor);
         Task<bool> EliminarProductoAsync(int idProducto, int idProveedor);
         //Task<ProductoDTO> ActualizarProductoAsync(int idProducto, UpdateProductoDTO dto, int idProveedor);
-        //Task<object> GetHomeDataAsync(int idProveedor);
-
+        Task<HomeProviderDataDTO> GetHomeDataAsync(int idProveedor);
+        Task<PriceRangeDTO> GetPriceRangeFromProductAsync(int noteId);
     }
 }
--- alquimia.Services/Services/Models/ProductDTO.cs
+++ alquimia.Services/Services/Models/ProductDTO.cs
@@ -2,7 +2,7 @@
 
 namespace backendAlquimia.Models
 {
-    public class ProductoDTO
+    public class ProductDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
