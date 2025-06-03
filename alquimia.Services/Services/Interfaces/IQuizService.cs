b90da481da24fbca057c46186406f5f9e86f2847
ANA LAURA NUnEZ <ananunez@alumno.unlam.edu.ar>
2025-06-03 04:57:57 -0300

    Desarrollo logica quiz

--- alquimia.Services/Services/Interfaces/IQuizService.cs
+++ alquimia.Services/Services/Interfaces/IQuizService.cs
@@ -1,16 +1,15 @@
-﻿using System;
+﻿using alquimia.Services.Services.Models;
+using backendAlquimia.alquimia.Services.Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using alquimia.Services.Services.Models;
 
 namespace alquimia.Services.Services.Interfaces
 {
     public interface IQuizService
     {
-        Task<List<QuestionDTO>> GetQuestionsAsync();
-        Task SaveAnswersAsync(List<AnswerDTO> respuestas);
-        Task<object?> GetResultAsync(List<AnswerDTO> respuestas);
+        QuizResponseDTO GetQuizResult(List<int> selectedOptions);
     }
 }
