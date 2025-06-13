80abf7496c166de656127e1c9ae69b5eb0dfa2d6
Axel <axeelleguero@gmail.com>
2025-06-13 16:18:56 -0300

    APP python con prints

--- alquimia.Api/Fastapi-accords/main.py
+++ alquimia.Api/Fastapi-accords/main.py
@@ -1,24 +1,18 @@
-﻿#namespace backendAlquimia.Fastapi_accords
-#{
-#    public class main
-#    {
-        
+﻿    
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 
 app = FastAPI()
 
-# Permitir acceso desde frontend en Next.js
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Cambiar por dominio real en producción
+    allow_origins=["http://localhost:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Cargar CSV al iniciar
 csv_path = "fra_cleaned.csv"
 df = pd.read_csv(csv_path, encoding="latin1", sep=";")
 
@@ -26,8 +20,6 @@ df = pd.read_csv(csv_path, encoding="latin1", sep=";")
 def search_perfume(name: str = Query(..., alias="q")):
     result = df[df["Perfume"].str.contains(name, case=False, na=False)]
 
-    #print("RESULTADO FILTRADO:", result[["Perfume", "mainaccord1", "mainaccord2", "mainaccord3"]].to_dict())
-    #perfume = result.iloc[0]
     if result.empty:
         return {"message": "Perfume not found", "notes": []}
     
@@ -37,11 +29,16 @@ def search_perfume(name: str = Query(..., alias="q")):
         perfume.get("mainaccord2"),
         perfume.get("mainaccord3")
     ]
+    # Imprimir los valores únicos de las columnas mainaccord1, mainaccord2 y mainaccord3
+    unique_accords = pd.concat([
+        df["mainaccord1"].dropna(),
+        df["mainaccord2"].dropna(),
+        df["mainaccord3"].dropna()
+    ]).unique().tolist()
+    print("Valores únicos de los acordes:", unique_accords)
     return {
         "perfume": perfume["Perfume"],
         "brand": perfume["Brand"],
         "notes": [n for n in notes if pd.notna(n)]
     }
 
-#    }
-#}
